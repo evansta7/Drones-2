3a1f69cf7f4d08643d3ba81152370aa5c2a7b949
evansta7 <lizy.vs@gmail.com>
2019-04-03 15:29:58 +0200

    Getting details from form

--- Classes/Login.js
+++ Classes/Login.js
@@ -1,5 +1,9 @@
 var hello = require('./CRUD.js');
 var dataReceived = [];
+function receiveName(){
+var userName = document.getElementById('loginEmail').value;
+alert(userName)
+}
 hello.data('*','Users', 'Username =\'liz\'', function(result){
   dataReceived = result;
   console.log(dataReceived);
