bebeb83c9fc6684326c3e1e2887d0b61acaa46bf
NicoleVanRooyen <39214278+NicoleVanRooyen@users.noreply.github.com>
2019-04-03 21:41:51 +0200

    baaasics - called variables for the register

---
+++ Classes/Register.js
@@ -0,0 +1,6 @@
+var username = document.getElementById('RegUsername').value;
+var password = document.getElementById('RegPassword').value;
+var email = document.getElementById('RegEmail').value;
+var cellphone = document.getElementById('RegCellphone').value;
+var name = document.getElementById('RegName').value;
+var surname = document.getElementById('RegSurname').value;
