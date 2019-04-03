7d75f3e5cd3797bfe95eb60b2577534b6f3aba64
evansta7 <lizy.vs@gmail.com>
2019-04-03 18:02:13 +0200

    Added insert function - It works

--- Classes/CRUD.js
+++ Classes/CRUD.js
@@ -10,8 +10,13 @@ var connection = mysql.createConnection({
 //This function selects the specified columns from the table in the database
 //A condition can be added if needed, else it can be left blank when calling this function
 //The Columns format should be as follow: Column1, Column2 etc
-//The tableName is the nme of the table you are quering
-//The
+//The tableName is the name of the table you are quering
+//The function returns the data in raw format, can be exported with the following code:
+/* var dbData = require('./CRUD.js');
+var dataReceived = [];
+dbData.data('*','Users', 'Username =\'liz\'', function(result){
+  dataReceived = result;
+}); */
 module.exports.data = function readData(columns,tableName , condition = "", callback){
   if (condition != "") {
    var query = 'SELECT ' + columns + 'FROM ' + tableName + ' WHERE ' + condition;
@@ -24,8 +29,26 @@ module.exports.data = function readData(columns,tableName , condition = "", call
      return callback(result);
     });
   });
-  
 }
+
+
+//This functions inserts data into the database
+//The SQL does not hava a condition
+//The columns is all the columns from the table
+//  - Columns for user:
+//    "`Username`, `Password`, `Email`, `CellphoneNumber`, `Name`, `Surname`"
+
+//  -Columns for farmer:
+//      ""`UserID`, `StreetAddress`, `Suburb`, `PostalCode`""
+
+//values must be in the same order as the column names
+function insertData(columns,values, tablename){
+    var query = 'INSERT INTO ' + tablename + ' (' + columns + ') VALUES(' + values + ')';
+    connection.connect(function(err){
+    connection.query(query);
+  })
+}
+insertData("`Username`, `Password`, `Email`, `CellphoneNumber`, `Name`, `Surname`",'\'liz\',\'123\',\'asd@com\',\'065455\',\'ELizabeth\', \'van Staden\'', 'users');
 
 
 
