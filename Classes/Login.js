0395d627c936d46d4879b4540d615148f1676b06
evansta7 <lizy.vs@gmail.com>
2019-04-03 21:57:23 +0200

    Working on navigating between pages

--- Classes/Login.js
+++ Classes/Login.js
@@ -1,10 +1,21 @@
-var hello = require('./CRUD.js');
-var dataReceived = [];
-function receiveName(){
-var userName = document.getElementById('loginEmail').value;
-alert(userName)
+var dbData = require('./CRUD.js');
+var userName = "";
+var password = "";
+
+function loginData(){
+  userName = document.getElementById('loginUsername').value;
+  password = document.getElementById('loginPassword').value;
+
+  if (userName == "" || password == "") {
+    document.getElementById('ErrorMessage').innerHTML = "<span style=\"color:red\">Please provide a username and password</span>";
+
+
+  }else{
+    document.getElementById('ErrorMessage').hidden = true;
+    window.location.assign("page-register.html");
+  } 
 }
-hello.data('*','Users', 'Username =\'liz\'', function(result){
+  dbData.data('*','Users', 'Username =\'liz\'', function(result){
   dataReceived = result;
   console.log(dataReceived);
 });
