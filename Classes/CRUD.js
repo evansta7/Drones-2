a5b37186608bd7f2bf59a419e1e17c43723e5911
evansta7 <lizy.vs@gmail.com>
2019-04-03 10:24:57 +0200

    Added Select function

--- Classes/CRUD.js
+++ Classes/CRUD.js
@@ -1,3 +1,5 @@
+
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -6,8 +8,27 @@ var connection = mysql.createConnection({
   database : 'dronefarming'
 });
 
-connection.connect(function(err) {
-  connection.query('SELECT * FROM dronefarming.users', function (err, result, fields) {
-    console.log(result);
+
+//This function selects the specified columns from the table in the database
+//A condition can be added if needed, else it can be left blank when calling this function
+//The Columns format should be as follow: Column1, Column2 etc
+//The tableName is the nme of the table you are quering
+//The
+function readData(columns,tableName , condition = ""){
+  var dataReceived = [];
+  if (condition != "") {
+   var query = 'SELECT ' + columns + 'FROM ' + tableName + ' WHERE ' + condition;
+  }else
+  {
+   var query = 'SELECT ' + columns + 'FROM ' + tableName;
+  }
+  connection.connect(function(err) {
+    connection.query(query, function (err, result, fields) {
+      dataReceived = result;
+    });
   });
-});
+  console.log(dataReceived);
+
+  return dataReceived;
+}
+var result = readData('*','Users', 'Username =\'liz\'');
