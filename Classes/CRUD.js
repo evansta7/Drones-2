8338b4ca0d72d2adffb185857735ccaaeb7b4d45
evansta7 <lizy.vs@gmail.com>
2019-04-03 09:40:59 +0200

    CRUD along with MySQL

--- Classes/CRUD.js
+++ Classes/CRUD.js
@@ -1,15 +1,13 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'dronefarming'
+});
 
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/Drones';
-
-function searchItem(collectionName, columnName, value){
-    MongoClient.connect(url,function(err,client){
-        var db = client.db('Drones');
-        db.collection(collectionName).find({[columnName]: value}).toArray(function(err,result){
-            console.log(result);
-        });
-        client.close();
-    });
-}
-
-searchItem('User', 'username', 'liz');
+connection.connect(function(err) {
+  connection.query('SELECT * FROM dronefarming.users', function (err, result, fields) {
+    console.log(result);
+  });
+});
